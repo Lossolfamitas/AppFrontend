7f2bfb7a1a29341b8f648218b5e6c9fe64b78025
Lossolfamitas <jc3458690@gmail.com>
2021-09-13 20:48:19 -0500

    Se realiza actualizacion del codigo para los tabs

--- Tienda/src/main/webapp/jsp/js/main.js
+++ Tienda/src/main/webapp/jsp/js/main.js
@@ -1,16 +1,15 @@
 $(document).ready(function(){
 	$('ul.tabs li a:first').addClass('active');
-	$('.secciones form').hide();
-	$('.secciones form:first').show();
+	$('.secciones article').hide();
+	$('.secciones article:first').show();
 
 	$('ul.tabs li a').click(function(){
 		$('ul.tabs li a').removeClass('active');
 		$(this).addClass('active');
-		$('.secciones form').hide();
+		$('.secciones article').hide();
 
 		var activeTab = $(this).attr('href');
 		$(activeTab).show();
 		return false;
 	});
 });
-
